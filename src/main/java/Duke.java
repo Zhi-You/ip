cd8fd2b4efe8b229f40248601ed81f5b086811e2
Zhi-You <tzyou98@gmail.com>
2020-08-27 15:48:40 +0800

    Add input and echo functionalities

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void printHorizontalLine() {
+        String horizontalLine = "________________________________________________";
+        System.out.println(horizontalLine);
+    }
+
+    // Echos commands entered by the user, and exits when the user types bye
+    public static void getInputAndEcho() {
+        Scanner in = new Scanner(System.in);
+        String command;
+
+        // Get initial input
+        command = in.nextLine();
+
+        while (!command.equalsIgnoreCase("bye")) {
+
+            printHorizontalLine();
+            // Echo out the command entered by the user
+            System.out.println(command);
+            printHorizontalLine();
+
+            // Gets another input
+            command = in.nextLine();
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -7,14 +35,14 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        String horizontalLine = "________________________________________________";
-
-        // Initial skeletal version that greets the user and exits
-        System.out.println(horizontalLine);
+        // Greet, Echo, Exit
+        printHorizontalLine();
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println(horizontalLine);
+        printHorizontalLine();
+        getInputAndEcho();
+        printHorizontalLine();
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontalLine);
+        printHorizontalLine();
     }
 }
