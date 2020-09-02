0cc3a9d815f64630d59928a038e8f924dbbb2ab6
Zhi-You <tzyou98@gmail.com>
2020-09-03 03:11:07 +0800

    Improve code quality mainly by refactoring code
    Followed closely to 'Code Quality' topics. Followed SLAP.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,7 +10,7 @@ public class Duke {
     private static final String COMMAND_DEADLINE_TASK = "deadline";
     private static final String COMMAND_EVENT_TASK = "event";
 
-    // To allow Duke to handle the commands specified by the user
+    // To allow Duke to handle the inputs specified by the user
     private static void handleUserInputs() {
         // Array of Task objects to store tasks specified by user
         Task[] tasks = new Task[MAX_NUMBER_OF_TASKS];
@@ -19,7 +19,6 @@ public class Duke {
         String userInput;
         String command;
         String taskDescription;
-
 
         // Continuously asks user for inputs until user exits
         while (true) {
@@ -32,35 +31,22 @@ public class Duke {
                 break;
             }
 
+            // Duke reads user commands and either show task list, mark task as done or add task
             switch (command) {
             case COMMAND_LIST:
                 DisplayManager.printList(tasks);
                 break;
             case COMMAND_MARK_TASK_DONE:
-                // Gets index of the task user specified to be done
-                int taskDoneIndex = Integer.parseInt(taskDescription.split(" ")[0]) - 1;
-
-                // Mark task as done
-                tasks[taskDoneIndex].markAsDone();
-
-                // Notifies user that the task is marked as done
-                DisplayManager.printMarkAsDoneMessage(tasks[taskDoneIndex]);
+                markTaskDone(tasks, taskDescription);
                 break;
             case COMMAND_TODO_TASK:
                 addTodoTask(tasks, taskDescription);
-
-                // Notifies user that task has been stored
-                DisplayManager.printTaskAddedMessage(tasks[Task.getTaskCount() - 1]);
                 break;
             case COMMAND_DEADLINE_TASK:
                 addDeadlineTask(tasks, taskDescription);
-
-                DisplayManager.printTaskAddedMessage(tasks[Task.getTaskCount() - 1]);
                 break;
             case COMMAND_EVENT_TASK:
                 addEventTask(tasks, taskDescription);
-
-                DisplayManager.printTaskAddedMessage(tasks[Task.getTaskCount() - 1]);
                 break;
             default:
                 DisplayManager.printMessageToUser("Please enter the correct command");
@@ -69,9 +55,20 @@ public class Duke {
         }
     }
 
+    private static void markTaskDone(Task[] tasks, String taskDescription) {
+        // Gets index of the task user specified to be done
+        int taskDoneIndex = Integer.parseInt(taskDescription.split(" ")[0]) - 1;
+
+        // Marks task as done
+        tasks[taskDoneIndex].markAsDone();
+
+        // Notifies user that the task is marked as done
+        DisplayManager.printMarkAsDoneMessage(tasks[taskDoneIndex]);
+    }
+
     // Extracts command word from user inputs to decide what Duke will do
     private static String getCommandFromInput(String userInput) {
-        // Split user input into different words to distinguish between command and task's information
+        // Splits user input into different words to distinguish between command and task's information
         String[] inputParts = userInput.split(" ");
 
         // User's command can be found in the first word of user's inputs
@@ -80,12 +77,12 @@ public class Duke {
 
     // Extracts task information from user inputs to decide what kind of task to add to list
     private static String getTaskDescriptionFromInput(String userInput) {
-        // Split user input into different words to distinguish between command and task's information
+        // Splits user input into different words to distinguish between command and task's information
         String[] inputParts = userInput.split(" ");
 
         String taskDescription = "";
 
-        // Get the informative parts from user input after the command (first word)
+        // Gets the informative parts from user input after the command (first word)
         for (int i = 1; i < inputParts.length; i++) {
             taskDescription = String.join(" ", taskDescription, inputParts[i]);
         }
@@ -94,29 +91,38 @@ public class Duke {
         return taskDescription;
     }
 
-    // Add ToDos typed task into the tasks array
+    // Adds ToDos typed task into the tasks array
     private static void addTodoTask(Task[] tasks, String taskDescription) {
         tasks[Task.getTaskCount()] = new Todo(taskDescription);
+
+        // Notifies user that task has been added
+        DisplayManager.printTaskAddedMessage(tasks[Task.getTaskCount() - 1]);
     }
 
-    // Add Deadlines typed task into the tasks array
+    // Adds Deadlines typed task into the tasks array
     private static void addDeadlineTask(Task[] tasks, String taskDescription) {
-        // Split user input to allow for retrieval of task details and deadline
+        // Splits user input to allow for retrieval of task details and deadline
         String[] deadlineTaskParts = taskDescription.split("/by");
         String description = deadlineTaskParts[0];
         String by = deadlineTaskParts[1];
 
         tasks[Task.getTaskCount()] = new Deadline(description, by);
+
+        // Notifies user that task has been added
+        DisplayManager.printTaskAddedMessage(tasks[Task.getTaskCount() - 1]);
     }
 
-    // Add Events typed task into the tasks array
+    // Adds Events typed task into the tasks array
     private static void addEventTask(Task[] tasks, String taskDescription) {
-        // Split user input to allow for retrieval of task details and event timing
+        // Splits user input to allow for retrieval of task details and event timing
         String[] eventTaskParts = taskDescription.split("/at");
         String description = eventTaskParts[0];
         String at = eventTaskParts[1];
 
         tasks[Task.getTaskCount()] = new Event(description, at);
+
+        // Notifies user that task has been added
+        DisplayManager.printTaskAddedMessage(tasks[Task.getTaskCount() - 1]);
     }
 
     public static void main(String[] args) {
