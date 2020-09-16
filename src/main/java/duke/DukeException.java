828892a36a2f0260398c397d36ce3d949041c0c7
Zhi-You <tzyou98@gmail.com>
2020-09-16 17:38:53 +0800

    Revert "Tweaks to touch up on week 5 work before working on week 6 work"
    This reverts commit 3ad498da7024453554e2ad1173abb1f2c0513407.

--- src/main/java/duke/DukeException.java
+++ src/main/java/duke/DukeException.java
@@ -3,7 +3,7 @@ package duke;
 // Using errorType string to differentiate what type of error we are facing with
 // To allow duke.DisplayManager to know what message to print through a switch statement
 // Note: initially wanted to use an enum but that was not an option anymore as we should pass
-// in a string instead to differentiate the error type
+// in a string instead.
 public class DukeException extends Exception{
     DukeException(String errorType) {
         super(errorType);
