f214fd9f7beb460c7df1ba32e2c7fdf95f257542
Tang Zhi You <tzyou98@gmail.com>
2020-09-25 21:22:36 +0800

    Revert "Branch level 8"

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,34 +1,14 @@
 package duke.task;
 
-import java.time.LocalDate;
-
 public class Deadline extends Task {
 
     private final String TASK_INDICATOR = "[D]";
 
-    private String deadline;
-
-    private LocalDate date;
+    protected String deadline;
 
     public Deadline(String description, String deadline) {
         super(description);
         this.deadline = deadline;
-    }
-
-    public String getDeadline() {
-        return deadline;
-    }
-
-    public void setDeadline(String deadline) {
-        this.deadline = deadline;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public LocalDate getDate() {
-        return date;
     }
 
     @Override
