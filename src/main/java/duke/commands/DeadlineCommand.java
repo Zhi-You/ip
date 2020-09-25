023ce8289e7b7e6d6973ce1d6ace82a6f1fab707
Zhi-You <tzyou98@gmail.com>
2020-09-25 15:37:47 +0800

    Add javadoc comments to Command, DeadlineCommand and DeleteCommand

--- src/main/java/duke/commands/DeadlineCommand.java
+++ src/main/java/duke/commands/DeadlineCommand.java
@@ -6,13 +6,26 @@ import duke.task.TaskList;
 
 import static duke.exception.ErrorTypeManager.ERROR_DEADLINE_EMPTY_DESCRIPTION;
 
+/**
+ * Adds a Deadline task to the user's task list.
+ */
 public class DeadlineCommand extends Command {
     public static final String COMMAND_WORD = "deadline";
 
+    /**
+     * @param taskDescription Description of the task and its deadline
+     */
     public DeadlineCommand(String taskDescription) {
         this.taskDescription = taskDescription;
     }
 
+    /**
+     * Executes the command and adds a Deadline task if its description is given
+     *
+     * @param taskList TaskList object with user's list of tasks and methods to operate on tasks
+     * @param storage Used to save user's tasks
+     * @throws DukeException If there is an error adding deadline task or saving tasks to the file
+     */
     @Override
     public void execute(TaskList taskList, Storage storage) throws DukeException {
         checkTaskDescriptionNotEmpty();
--- src/main/java/duke/commands/DeleteCommand.java
+++ src/main/java/duke/commands/DeleteCommand.java
@@ -6,14 +6,26 @@ import duke.task.TaskList;
 
 import static duke.exception.ErrorTypeManager.ERROR_DELETETASK_EMPTY_DESCRIPTION;
 
-
+/**
+ * Deletes a task from the user's task list.
+ */
 public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
 
+    /**
+     * @param taskDescription Description of the task to be deleted in the form of its index
+     */
     public DeleteCommand(String taskDescription) {
         this.taskDescription = taskDescription;
     }
 
+    /**
+     * Executes the command and deletes a task if its description is given
+     *
+     * @param taskList TaskList object with user's list of tasks and methods to operate on tasks
+     * @param storage Used to save user's tasks
+     * @throws DukeException If there is an error deleting a task or saving the remaining tasks to the file
+     */
     @Override
     public void execute(TaskList taskList, Storage storage) throws DukeException {
         checkTaskDescriptionNotEmpty();
